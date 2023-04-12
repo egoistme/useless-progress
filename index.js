65fc923b4d09c6fa57d8884d48dfaa789ea2a5f1
yangdehui <yangdehui@megaview.com>
2023-04-13 02:02:48 +0800

    fix: the end number will be 1

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 class UselessProgress {
     constructor(config = {}) {
         this.progress = 0;
-        this.count = 0;
         this.onprogress = config.onprogress;
         this.extremeTime = config.extremeTime || 60000;
+        this.frequency = config.frequency || 200;
+        this._count = 0;
         this._interval = null;
-        this._coefficient = 461 / this.extremeTime;
-
+        this._coefficient = 4.61 * this.frequency / this.extremeTime;
     }
 
     getProgress() {
@@ -18,9 +18,8 @@ class UselessProgress {
     }
 
     start() {
-        this._startTime = new Date().getTime();
         this.progress = 0
-        this._interval = setInterval(this._onInterval.bind(this), 100);
+        this._interval = setInterval(this._onInterval.bind(this), this.frequency);
     }
     stop() {
         clearInterval(this._interval);
@@ -32,7 +31,11 @@ class UselessProgress {
     }
 
     _onInterval() {
-        this.setProgress(1 - Math.exp(-this.count++ * this._coefficient))
+        let count = 1 - Math.exp(-this._count++ * this._coefficient);
+        if (count === 1) {
+            count = 0.9999999999999999;
+        }
+        this.setProgress(count)
         if (this.onprogress) {
             this.onprogress(this.getProgress());
         }
